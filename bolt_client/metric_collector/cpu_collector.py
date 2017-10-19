33cc8c433e62c84c59d064d76ad92a5374f6a623
Saurabh Badhwar <sbadhwar@redhat.com>
2017-10-19 11:55:37 +0530

    Add the CPU collector
    Signed-off-by: Saurabh Badhwar <sbadhwar@redhat.com>

---
+++ bolt_client/metric_collector/cpu_collector.py
@@ -0,0 +1,45 @@
+'''
+File: cpu_collector.py
+Description: CPU usage collector
+Author: Saurabh Badhwar <sbadhwar@redhat.com>
+Date: 18/10/2017
+'''
+from structures import Metric
+import psutil
+import time
+
+class CpuCollector(Metric):
+    """CPU Metrics collector
+
+    Collects the CPU metrics such as percent utilization, IOwait, nice and IRQ
+    """
+
+    def __init__(self, sampling_rate=5):
+        """Initialize the CpuCollector instance
+
+        Keyword arguments:
+        sampling_rate -- The time interval between successive data collection
+        """
+
+        super(CpuCollector, self).__init__('cpu', sampling_rate)
+
+    def start_sampling(self):
+        """Start sampling the data"""
+
+        cpu_times = psutil.cpu_times()
+        user = cpu_times.user
+        system = cpu_times.system
+        nice = cpu_times.nice
+        idle = cpu_times.idle
+        iowait = cpu_times.iowait
+        irq = cpu_times.irq
+        softirq = cpu_times.softirq
+
+        timestamp = time.time()
+        self.record_data('user', timestamp, user)
+        self.record_data('system', timestamp, system)
+        self.record_data('nice', timestamp, nice)
+        self.record_data('idle', timestamp, idle)
+        self.record_data('iowait', timestamp, iowait)
+        self.record_data('irq', timestamp, irq)
+        self.record_data('softirq', timestamp, softirq)
