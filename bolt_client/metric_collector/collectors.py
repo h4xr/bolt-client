b5f1ec7f43c3b8cecc5773f4b87b19bdc968f42b
Saurabh Badhwar <sbadhwar@redhat.com>
2017-10-21 14:12:11 +0530

    Implement the memory collector
    Signed-off-by: Saurabh Badhwar <sbadhwar@redhat.com>

--- bolt_client/metric_collector/collectors.py
+++ bolt_client/metric_collector/collectors.py
@@ -27,6 +27,7 @@ class CpuCollector(Metric):
         """Start sampling the data"""
 
         cpu_times = psutil.cpu_times()
+        timestamp = time.time()
         user = cpu_times.user
         system = cpu_times.system
         nice = cpu_times.nice
@@ -35,7 +36,6 @@ class CpuCollector(Metric):
         irq = cpu_times.irq
         softirq = cpu_times.softirq
 
-        timestamp = time.time()
         self.record_data('user', timestamp, user)
         self.record_data('system', timestamp, system)
         self.record_data('nice', timestamp, nice)
@@ -43,3 +43,35 @@ class CpuCollector(Metric):
         self.record_data('iowait', timestamp, iowait)
         self.record_data('irq', timestamp, irq)
         self.record_data('softirq', timestamp, softirq)
+
+class MemoryCollector(Metric):
+    """Memory metrics collector
+
+    Collects the memory metrics such as total memory, available and used memory,
+    etc.
+    """
+
+    def __init__(self, sampling_rate=5):
+        """Initialize the Memory collector instance
+
+        Keyword arguments:
+        sampling_rate -- The time interval between successive data collections
+        """
+
+        super(MemoryCollector, self).__init__('memory', sampling_rate)
+
+    def start_sampling(self):
+        """Start the data collection and recording of the data"""
+
+        virtual_memory = psutil.virtual_memory()
+        timestamp = time.time()
+
+        used_mem = virtual_memory.used
+        free_mem = virtual_memory.free
+        cached_mem = virtual_memory.cached
+        shared_mem = virtual_memory.shared
+
+        self.record_data('used', timestamp, used_mem)
+        self.record_data('free', timestamp, free_mem)
+        self.record_data('cached', timestamp, cached_mem)
+        self.record_data('shared', timestamp, shared_mem)
